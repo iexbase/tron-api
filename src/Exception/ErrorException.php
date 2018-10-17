7f54d23dedb5571f0a590f06b911f7e1183a597c
serderovsh <steein.shamsudin@gmail.com>
2018-10-17 20:43:47 +0300

    added ErrorException

---
+++ src/Exception/ErrorException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: steei
+ * Date: 17.10.2018
+ * Time: 20:16
+ */
+
+namespace IEXBase\TronAPI\Exception;
+
+
+class ErrorException extends \ErrorException
+{
+
+}
